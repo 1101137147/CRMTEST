a34258e536305f695fad7beaded08b1aabd01dac
1101137147 <john830426000@yahoo.com.tw>
2015-12-17 14:29:56 +0800

    v2

--- db.php
+++ db.php
@@ -7,9 +7,7 @@ $dbname = 'test';
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
     $conn->exec("set name utf8");
-   // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
-//$conn = null;
 ?>
